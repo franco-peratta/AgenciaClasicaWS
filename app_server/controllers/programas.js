2705df857cac12b7fcf54a86b08997d90170d73f
Franco Peratta <franco.peratta@hotmail.com>
2020-02-15 19:50:00 -0300

    First commit!
    Let it be the first of many :)

--- app_server/controllers/programas.js
+++ app_server/controllers/programas.js
@@ -2,10 +2,12 @@ const Programa = require('../models/programa');
 
 const programas_get = function (req, res) {
 
-    const programas = Programa.find().then((data) => {
+    Programa.find().then((data) => {
         res.status(200).json(data);
-    });
-
+    })
+        .catch(err => {
+            res.status(500).send("No se pudo obtener los programas");
+        });
 };
 
 module.exports = { programas_get };
